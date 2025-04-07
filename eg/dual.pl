399752a6494f5b7c8242d5c1502abbcd6aee98c4
Gene Boggs <gene@ology.net>
2025-04-07 14:54:05 -0700

    Time

--- eg/dual.pl
+++ eg/dual.pl
@@ -23,7 +23,7 @@ $filter1->control(1); # mod-wheel
 # $filter1->range_bottom(10);
 # $filter1->range_top(100);
 # $filter1->range_step(2);
-# $filter1->time_step(0.4);
+$filter1->time_step(0.25);
 # $filter1->step_up(10);
 # $filter1->step_down(2);
 
@@ -31,7 +31,7 @@ $filter2->control(22); # noise
 $filter2->range_bottom(0);
 $filter2->range_top(80);
 # $filter2->range_step(2);
-# $filter2->time_step(0.5);
+$filter2->time_step(0.5);
 # $filter2->step_up(10);
 # $filter2->step_down(2);
 
@@ -39,7 +39,7 @@ $filter3->control(13); # delay time
 # $filter3->range_bottom(10);
 # $filter3->range_top(100);
 # $filter3->range_step(2);
-# $filter3->time_step(0.4);
+$filter3->time_step(0.5);
 # $filter3->step_up(10);
 # $filter3->step_down(2);
 
