ed585b7ca033da04b59407400e3bffc5992e8463
Gene Boggs <gene@ology.net>
2025-04-13 00:58:44 -0700

    Multi

--- eg/multi.pl
+++ eg/multi.pl
@@ -6,14 +6,25 @@ use curry;
 use MIDI::RtController ();
 use MIDI::RtController::Filter::CC ();
 
-my $input_name  = shift || 'joystick';
-my $output_name = shift || 'usb';
+my $input_names = shift || 'keyboard,pad,joystick'; # midi controller devices
+my $output_name = shift || 'usb'; # midi output
+
+my $inputs = [ split /,/, $input_names ];
 
 my $control = MIDI::RtController->new(
-    input   => $input_name,
+    input   => $inputs->[0],
     output  => $output_name,
     verbose => 1,
 );
+
+for my $name (@$inputs[1 .. $#$inputs]) {
+    MIDI::RtController->new(
+        input    => $name,
+        loop     => $control->loop,
+        midi_out => $control->midi_out,
+        verbose  => 1,
+    );
+}
 
 my %filters = (
     # 1 => { # mod-wheel
