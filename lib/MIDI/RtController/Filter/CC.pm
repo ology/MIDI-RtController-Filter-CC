cab3a93efd9a9549a32321d7a1b7995c7e7603c3
Gene Boggs <gene@ology.net>
2025-04-14 19:07:13 -0700

    Default

--- lib/MIDI/RtController/Filter/CC.pm
+++ lib/MIDI/RtController/Filter/CC.pm
@@ -315,7 +315,8 @@ sub single ($self, $device, $dt, $event) {
     my ($ev, $chan, $note, $val) = $event->@*;
     return 0 if defined $self->trigger && $note != $self->trigger;
 
-    my $cc = [ 'control_change', $self->channel, $self->control, $self->value ];
+    my $value = $self->value || $val;
+    my $cc = [ 'control_change', $self->channel, $self->control, $value ];
     $self->rtc->send_it($cc);
     return 0;
 }
