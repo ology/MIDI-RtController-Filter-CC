a0ed8491038cd3cddd3907e5cb598d64f346538c
Gene Boggs <gene@ology.net>
2025-05-25 01:25:22 -0700

    WIP

--- eg/control-change.pl
+++ eg/control-change.pl
@@ -6,7 +6,7 @@ use MIDI::RtController::Filter::CC ();
 
 my $input_name  = shift || 'joystick';
 my $output_name = shift || 'usb';
-my $filter_name = shift || 'single';
+my $filter_name = shift || 'breathe';
 
 my $controller = MIDI::RtController->new(
     input   => $input_name,
@@ -16,20 +16,20 @@ my $controller = MIDI::RtController->new(
 
 my $filter = MIDI::RtController::Filter::CC->new(rtc => $controller);
 
-$controller->send_it(['patch_change', $filter->channel, 6]);
+# $controller->send_it(['patch_change', $filter->channel, 6]);
 
 # $filter->trigger(25);
-# $filter->control(1); # CC#01 = mod-wheel
+$filter->control(1); # CC#01 = mod-wheel
 # $filter->value(0);
 # $filter->range_bottom(0);
 # $filter->range_top(70);
 # $filter->range_step(2);
-# $filter->time_step(0.15);
+# $filter->time_step(60 / (60 * 24)); # seconds / (bpm * ppqn)
 # $filter->step_up(10);
 # $filter->step_down(2);
 
-# my $method = "curry::$filter_name";
-# $controller->add_filter($filter_name, all => $filter->$method);
+my $method = "curry::$filter_name";
+$controller->add_filter($filter_name, all => $filter->$method);
 
 $controller->run;
 
