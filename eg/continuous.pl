7701bbda14838df276ada5c056ad8864b10adfc2
Gene Boggs <gene.boggs@gmail.com>
2026-08-20 08:37:32 -0700

    Ctl

--- eg/continuous.pl
+++ eg/continuous.pl
@@ -3,25 +3,22 @@
 use MIDI::RtController ();
 use MIDI::RtController::Filter::CC ();
 
-my $in  = shift || 'pad'; # midi input controller
-my $out = shift || 'usb'; # midi output
+my $in  = shift || 'pad';        # midi input controller
+my $out = shift || 'usb';        # midi output
+my $ctl = shift || '12=74,13=71' # trigger=control,...
 
-my @filters = (
-    {
-        port => $in,
-        event => 'control_change',
-        control => 74, # CUTOFF
-        trigger => 12, # X axis
-    },
-    {
-        port => $in,
-        event => 'control_change',
-        control => 71, # RESONANCE
-        trigger => 13, # Y axis
-    },
-);
+my @ctl = split /,/, $ctl;
+my %ctl = map { split /=/, $_ } @ctl;
 
-# open the inputs
+my @filters = map {
+    +{
+        port  => $in,
+        event => 'control_change',
+        trigger => $_,
+        control => $ctl{$_},
+    }
+}
+
 my $controller = MIDI::RtController->new(
     input   => $in,
     output  => $out,
